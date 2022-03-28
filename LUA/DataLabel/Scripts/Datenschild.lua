f1649c58bad84430b05216c852907fa6f41c354b
Seilscheibe <78691800+Seilscheibe@users.noreply.github.com>
2022-03-28 16:19:35 +0200

    Update and rename exampleConstuctor.lua to Datenschild.lua

---
+++ LUA/DataLabel/Scripts/Datenschild.lua
@@ -0,0 +1,16 @@
+--
+-- author: seilscheibe
+--
+-- date: 25/03/22
+--
+-- filename: Datenschild.lua
+--
+-- reuse allowed, but at least mention me in your mod/credits ^^
+--
+-- if you have any questions feel free to contact me, Discord - @Seilscheibe #4748
+--
+-- detailed instructions can be found on: https://github.com/Seilscheibe/WinterResortSimulator
+
+Datenschild             = Datenschild or {};
+
+-- available soon -- 
--- LUA/DataLabel/Scripts/exampleConstuctor.lua
+++
@@ -1,8 +0,0 @@
--------------------------------------------------
--- author: Seilscheibe RDA
--- date: 29/08/21
--- filename: exampleConstuctor.lua
--- notes: open-surce - visible
--------------------------------------------------
-
-available soon!
