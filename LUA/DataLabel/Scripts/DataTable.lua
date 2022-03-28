3244b40b8a3e614d40074c78d6cbd555c79fdbaa
Seilscheibe <78691800+Seilscheibe@users.noreply.github.com>
2022-03-28 16:21:07 +0200

    Update and rename exampleTable.lua to DataTable.lua

---
+++ LUA/DataLabel/Scripts/DataTable.lua
@@ -0,0 +1,15 @@
+
+--
+-- author: seilscheibe
+--
+-- date: 25/03/22
+--
+-- filename: DataTable.lua
+--
+-- reuse allowed, but at least mention me in your mod/credits ^^
+--
+-- if you have any questions feel free to contact me, Discord - @Seilscheibe #4748
+--
+-- detailed instructions can be found on: https://github.com/Seilscheibe/WinterResortSimulator
+
+-- available soon -- 
--- LUA/DataLabel/Scripts/exampleTable.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------
--- author: Seilscheibe RDA
--- date: 29/08/21
--- filename: exampleTable.lua
--- notes: open-surce - visible
--------------------------------------------------
-
-
-available soon!
